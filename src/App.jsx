155e0de11febed54372193e5666b7e53bcb8fd7a
Khaled Hamdy <khaledhamdy609@gmail.com>
2023-10-18 23:12:09 +0300

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+
+export default function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline text-center bg-slate-500">Hello world!</h1> 
+    </>
+  )
+}
+
+
